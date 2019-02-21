b9f95a723f141b0e37b34bba05061728dfb09eca
Pierre Brisorgueil <pierrebrisorgueil@me.com>
2019-02-21 12:24:30 +0100

    :shirt: init swiftlint (best solution, compare from stars and some read)

--- waosSwiftTests/waosSwiftTests.swift
+++ waosSwiftTests/waosSwiftTests.swift
@@ -9,7 +9,7 @@
 import XCTest
 @testable import waosSwift
 
-class waosSwiftTests: XCTestCase {
+class WaosSwiftTests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
