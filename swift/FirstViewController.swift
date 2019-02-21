13d6e4ae22a127704ebec339ab9fc520cc523347
Pierre Brisorgueil <pierrebrisorgueil@me.com>
2019-02-21 10:29:19 +0100

    :tada: init tab bar application

---
+++ swift/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  swift
+//
+//  Created by pierre brisorgueil on 21/02/2019.
+//  Copyright © 2019 WeAreOpenSource. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ swift/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  swift
+//
+//  Created by pierre brisorgueil on 21/02/2019.
+//  Copyright © 2019 WeAreOpenSource. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
