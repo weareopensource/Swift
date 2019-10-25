539bf5440b9a05fd2db654a4462d510c884b0fed
Pierre Brisorgueil <pierrebrisorgueil@me.com>
2019-10-25 09:46:58 +0200

    feat(core): begin router automation ✨

---
+++ waosSwift/lib/helpers/Extensions/L10n.swift
@@ -0,0 +1,7 @@
+extension L10n {
+   static func get(_ table: String, _ key: String) -> String {
+    // swiftlint:disable:next nslocalizedstring_key
+    let format = NSLocalizedString(key, tableName: table, comment: "")
+    return String(format: format, locale: Locale.current)
+  }
+}
