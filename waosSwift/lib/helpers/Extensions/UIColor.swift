8b6be041772dd416fa5d9639468c5dd679280e16
Pierre Brisorgueil <pierrebrisorgueil@me.com>
2019-11-19 15:43:48 +0100

    style(helpers): add letters/line label blur/darker/ligther img 💄

--- waosSwift/lib/helpers/Extensions/UIColor.swift
+++ waosSwift/lib/helpers/Extensions/UIColor.swift
@@ -4,14 +4,26 @@
 
 extension UIColor {
 
+    /**
+     * @desc lighter color
+     * @param {CGFloat} percentage,
+     */
     func lighter(by percentage: CGFloat = 30.0) -> UIColor? {
         return self.adjust(by: abs(percentage) )
     }
 
+    /**
+     * @desc darker color
+     * @param {CGFloat} percentage,
+     */
     func darker(by percentage: CGFloat = 30.0) -> UIColor? {
         return self.adjust(by: -1 * abs(percentage) )
     }
 
+    /**
+     * @desc adjust color darkness / lightness from coefficient
+     * @param {CGFloat} percentage,
+     */
     func adjust(by percentage: CGFloat = 30.0) -> UIColor? {
         var red: CGFloat = 0, green: CGFloat = 0, blue: CGFloat = 0, alpha: CGFloat = 0
         if self.getRed(&red, green: &green, blue: &blue, alpha: &alpha) {
---
+++ waosSwift/lib/helpers/Extensions/UIImage.swift
@@ -0,0 +1,52 @@
+extension UIImage {
+    /**
+     * @desc set blur effect on UIImageView with a radius coefficient
+     * @param {CGFloat} radius,
+     */
+    func blurred(radius: CGFloat) -> UIImage {
+        let ciContext = CIContext(options: nil)
+        guard let cgImage = cgImage else { return self }
+        let inputImage = CIImage(cgImage: cgImage)
+        guard let clampFilter = CIFilter(name: "CIAffineClamp") else { return self }
+        clampFilter.setDefaults()
+        clampFilter.setValue(inputImage, forKey: kCIInputImageKey)
+        guard let ciFilter = CIFilter(name: "CIGaussianBlur") else { return self }
+        ciFilter.setValue(clampFilter.outputImage, forKey: kCIInputImageKey)
+        ciFilter.setValue(radius, forKey: "inputRadius")
+        guard let resultImage = ciFilter.value(forKey: kCIOutputImageKey) as? CIImage else { return self }
+        guard let cgImage2 = ciContext.createCGImage(resultImage, from: inputImage.extent) else { return self }
+        return UIImage(cgImage: cgImage2)
+    }
+
+    /**
+     * @desc lighter image
+     * @param {CGFloat} percentage,
+     */
+    func lighter(by percentage: CGFloat = 30) -> UIImage? {
+        return self.adjust(by: abs(percentage) )
+    }
+
+    /**
+     * @desc darker image
+     * @param {CGFloat} percentage,
+     */
+    func darker(by percentage: CGFloat = 30) -> UIImage? {
+        return self.adjust(by: -1 * abs(percentage) )
+    }
+
+    /**
+     * @desc adjust image darkness / lightness from coefficient
+     * @param {CGFloat} percentage,
+     */
+    func adjust(by percentage: CGFloat = 30) -> UIImage? {
+        let ciContext = CIContext(options: nil)
+        guard let cgImage = cgImage else { return self }
+        let inputImage = CIImage(cgImage: cgImage)
+        guard let ciFilter = CIFilter(name: "CIExposureAdjust") else { return self }
+        ciFilter.setValue(inputImage, forKey: "inputImage")
+        ciFilter.setValue(percentage/100, forKey: "inputEV")
+        guard let resultImage = ciFilter.value(forKey: kCIOutputImageKey) as? CIImage else { return self }
+        guard let cgImage2 = ciContext.createCGImage(resultImage, from: inputImage.extent) else { return self }
+        return UIImage(cgImage: cgImage2)
+    }
+}
