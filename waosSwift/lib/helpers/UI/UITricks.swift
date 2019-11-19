484b98608b2770263fb3825bdf85ac8d65345175
Pierre Brisorgueil <pierrebrisorgueil@me.com>
2019-11-19 10:44:27 +0100

    style(helpers): init helper UITricks, begin by makeCircular function 💄

---
+++ waosSwift/lib/helpers/UI/UITricks.swift
@@ -0,0 +1,8 @@
+/**
+* @desc make circular view, must be called in override func layoutSubviews
+* @param {UIView} _view,
+*/
+func makeCircular(_ view: UIView) {
+    view.layer.cornerRadius = view.bounds.size.width / 2.0
+    view.clipsToBounds = true
+}
