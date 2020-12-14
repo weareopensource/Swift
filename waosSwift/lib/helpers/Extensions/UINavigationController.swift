a4e6f2f7ded7ad5c48f0b8d11764a821cd842d4e
Pierre Brisorgueil <pierrebrisorgueil@me.com>
2020-12-14 17:50:25 +0100

    feat(users,home): review page design if no action ✨

---
+++ waosSwift/lib/helpers/Extensions/UINavigationController.swift
@@ -0,0 +1,12 @@
+extension UINavigationController {
+    /**
+     * @desc clear navigation controller background
+     */
+    func clear() {
+        self.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        self.navigationBar.shadowImage = UIImage()
+        self.navigationBar.isTranslucent = true
+        self.view.backgroundColor = .clear
+        self.navigationBar.backgroundColor = .clear
+    }
+}
