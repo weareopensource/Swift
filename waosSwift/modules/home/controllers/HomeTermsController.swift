d11662eb30d128ee076c55ffe2af8deef107c2cc
Pierre Brisorgueil <pierrebrisorgueil@me.com>
2021-04-12 11:52:28 +0200

    refactor(global): error handling manager ♻️

--- waosSwift/modules/home/controllers/HomeTermsController.swift
+++ waosSwift/modules/home/controllers/HomeTermsController.swift
@@ -90,7 +90,7 @@ private extension HomeTermsController {
                     mvc.mailComposeDelegate = self
                     mvc.setToRecipients([(config["app"]["mails"]["report"].string ?? "")])
                     mvc.setSubject(L10n.userReport)
-                    mvc.setMessageBody(setMailError(reactor.currentState.error?.source), isHTML: true)
+                    mvc.setMessageBody(setMailError("\(reactor.currentState.error?.title ?? "") \n \(reactor.currentState.error?.description ?? "") \n  \(reactor.currentState.error?.source ?? "")"), isHTML: true)
                     self.present(mvc, animated: true)
                 }
             })
@@ -129,7 +129,7 @@ private extension HomeTermsController {
             .filterNil()
             .throttle(.milliseconds(Metric.timesButtonsThrottle), latest: false, scheduler: MainScheduler.instance)
             .subscribe(onNext: { error in
-                self.error.configureContent(title: error.title, body: error.description)
+                self.error.configureContent(title: error.title, body: error.description == "" ? error.title : error.description)
                 self.error.button?.isHidden = (error.source != nil && error.code != 401) ? false : true
                 SwiftMessages.hideAll()
                 SwiftMessages.show(config: self.popupConfig, view: self.error)
